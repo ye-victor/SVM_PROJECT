ba2e83c0c368d1424f583c2fb192f711fa172bde
VictorYeGitHub <victorye.contact@gmail.com>
2019-11-06 20:36:58 +0100

    .

--- app.R
+++ app.R
@@ -10,7 +10,8 @@ library(ineq)
 
 
 set.seed(12345)
-creditcard <- read.csv("/Users/Victor/Desktop/SVM Shiny/SVMS/creditcard.csv")
+#creditcard <- read.csv("/Users/Victor/Desktop/SVM Shiny/SVMS/creditcard.csv")
+creditcard <- readRDS(file=url("https://raw.githubusercontent.com/VictorYeGitHub/SVM_PROJECT/master/creditcard.rds"))
 creditcard$Class <- factor(creditcard$Class, levels=c("0","1"))
 creditcard <- creditcard[sample(nrow(creditcard)),]
 smote_data <- SMOTE(Class ~ ., data  = creditcard, perc.over = 300, perc.under = 550, k=5)
