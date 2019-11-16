72557c075394c70bf9545e530e8a693c6c6c866a
VictorYeGitHub <victorye.contact@gmail.com>
2019-11-16 23:48:22 +0100

    main update

--- app.R
+++ app.R
@@ -52,7 +52,9 @@ ui <- fluidPage(
                                                  tabPanel("Français",includeHTML("Introduction.html")))),
                  tabPanel(p(icon("wrench"),"Use of the system"),tabsetPanel(tabPanel("English", includeHTML("utilisationen.html")),
                                                                       tabPanel("Français",includeHTML("utilisation.html")))),
-                 tabPanel(p(icon("calculator"),"Case study"),
+               tabPanel(p(icon("check"),"Measuring tools"), tabsetPanel(tabPanel("English", includeHTML("explainen.html")),
+                                                                        tabPanel("Français",includeHTML("explain.html"))) ),
+               tabPanel(p(icon("calculator"),"Case study"),
                           sidebarLayout(
                     sidebarPanel(selectInput(inputId='kernel', label='Choose a kernel to apply to the SVM',
                                              choices=c('Linear','Radial','Polynomial','Sigmoid'), multiple = FALSE, selected='Radial'),
@@ -79,10 +81,9 @@ ui <- fluidPage(
                                  
                                  
                                  ,
-                    mainPanel(plotOutput("ROCSVM"),includeHTML("logo.html")))),
-                 tabPanel(p(icon("check"),"SVM vs. other models"),
-                          tabsetPanel(tabPanel("English", includeHTML("explainen.html")),
-                                      tabPanel("Français",includeHTML("explain.html"))),
+                    mainPanel(plotOutput("ROCSVM"),plotOutput("CONFSVM"),includeHTML("logo.html")))),
+               
+                 tabPanel(p(icon("balance-scale"),"SVM vs. other models"),
                           h2("Comparison of the different models"),
                           splitLayout(plotOutput("ROCCOMP"),plotOutput("GAINCOMP")),
                           sidebarLayout(
@@ -110,8 +111,8 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  #creditcard <- read.csv("/Users/Victor/Desktop/creditcard.csv")
-  creditcard <- readRDS(file=url("https://raw.githubusercontent.com/VictorYeGitHub/SVM_PROJECT/master/creditcard.rds"))
+  creditcard <- read.csv("/Users/Victor/Desktop/creditcard.csv")
+  #creditcard <- readRDS(file=url("https://raw.githubusercontent.com/VictorYeGitHub/SVM_PROJECT/master/creditcard.rds"))
   creditcard$Class <- factor(creditcard$Class, levels=c("0","1"))
   
   set.seed(2019)
@@ -120,23 +121,26 @@ server <- function(input, output, session) {
   train = creditcard[samp,]
   testSplit = creditcard[-samp,] 
   
-  fraudData= train[train$Class=="1",]
-  NoFraudData=train[train$Class=="0",]
-  NoFraudData=NoFraudData[1:1180,]
+  fraudData= train[train$Class==1,]
+  NoFraudData=train[train$Class==0,]
+  NoFraudData=NoFraudData[1:800,]
   trainSplit=rbind(NoFraudData,fraudData)
   
-  svm.model <- svm(Class ~ ., data=trainSplit, kernel="radial", cost=10, gamma=0.1)
-  svm.predict <- predict(svm.model,testSplit)
+  
+  svm.model <- svm(Class~., data=trainSplit, kernel="radial", cost=10, gamma=0.1, probability=TRUE)
+  svm.predict <- predict(svm.model,newdata=testSplit,type="class",probability=TRUE)
+
   
   log.model <- glm(Class~.,trainSplit,family="binomial")
   log.predict <- predict(log.model,testSplit,type="response")
   fitted.results <- ifelse(log.predict > 0.5,1,0)
   
-  knn.model = knn(trainSplit[,-31],testSplit[,-31],trainSplit$Class,k=17)
+  knn.model = knn(trainSplit[,-31],testSplit[,-31],trainSplit$Class,k=13)
   
-  rf.model <- randomForest(Class~.,trainSplit , mtry=1, importance = TRUE)
+  
+  rf.model <- randomForest(Class~.,trainSplit , ntree=180, importance = TRUE)
   rf.pred <- predict(rf.model, testSplit)
-  
+
   
   
   cost_model = function(predicted.classes, true.classes, amounts, fixedcost) {
@@ -146,32 +150,27 @@ server <- function(input, output, session) {
   }
   
   numsvm=as.numeric(svm.predict)
-  zz <- as.character(numsvm)
-  zz[zz == "1"] <- "0"
-  zz[zz == "2"] <- "1"
-  zz=as.numeric(zz)
-  costsvm=cost_model(zz,as.numeric(testSplit$Class), testSplit$Amount, fixedcost=10)
-  
+  numsvm[numsvm == 1] <- 0
+  numsvm[numsvm == 2] <- 1
   
   numfit=as.numeric(fitted.results)
-  costfit=cost_model(numfit,as.numeric(testSplit$Class), testSplit$Amount, fixedcost=10)
-  
   
   numknn=as.numeric(knn.model)
-  dd <- as.character(numknn)
-  dd[dd == "1"] <- "0"
-  dd[dd == "2"] <- "1"
-  dd=as.numeric(dd)
-  costknn=cost_model(dd,as.numeric(testSplit$Class), testSplit$Amount, fixedcost=10)
+  numknn[numknn == 1] <- 0
+  numknn[numknn == 2] <- 1
   
   numrf=as.numeric(rf.pred)
-  tt <- as.character(numrf)
-  tt[tt == "1"] <- "0"
-  tt[tt == "2"] <- "1"
-  tt=as.numeric(tt)
-  costrf=cost_model(tt,as.numeric(testSplit$Class), testSplit$Amount, fixedcost=10)
+  numrf[numrf == 1] <- 0
+  numrf[numrf == 2] <- 1
   
+  numtest=as.numeric(testSplit$Class)
+  numtest[numtest==1] <- 0
+  numtest[numtest==2] <- 1
   
+  costsvm=cost_model(numsvm,numtest, testSplit$Amount, fixedcost=10)
+  costfit=cost_model(numfit,numtest, testSplit$Amount, fixedcost=10)
+  costknn=cost_model(numknn,numtest, testSplit$Amount, fixedcost=10)
+  costrf=cost_model(numrf,numtest, testSplit$Amount, fixedcost=10)
   
   hide(id = "loading-content", anim = TRUE, animType = "fade")    
   show("app-content")
@@ -233,6 +232,90 @@ roc(testSplit$Class,as.numeric(svm.predict),plot=T,main="SVM with radial kernel"
     roc(testSplit$Class,as.numeric(svm.predict),plot=T,main="SVM with sigmoid kernel",legacy.axes=TRUE,percent=TRUE,xlab="False Positive Percentage",ylab="True Positive Percentage",col="#377eb8",lwd=4,print.auc=T,print.auc.y=30, print.auc.x=25)
   }
 })  
+
+
+output$CONFSVM <- renderPlot ({ 
+  
+  if (x()=="Radial"){
+    svm.model <- svm(Class ~ ., data=trainSplit, kernel="radial", cost=input$costrad, gamma=input$gammrad)
+    svm.predict <- predict(svm.model,testSplit)
+  }
+  
+  if (x()=="Linear"){
+    svm.model <- svm(Class ~ ., data=trainSplit, kernel="linear", cost=5)
+    svm.predict <- predict(svm.model,testSplit)
+  }
+  
+  if (x()=="Polynomial"){
+    svm.model <- svm(Class ~ ., data=trainSplit, kernel="polynomial", degree=input$ppoly,coef0=input$cpoly, cost=input$costpoly)
+    svm.predict <- predict(svm.model,testSplit)
+  }
+  
+  if (x()=="Sigmoid"){
+    svm.model <- svm(Class ~ ., data=trainSplit, kernel="sigmoid", gamma=input$teta1sigmoid, coef0=input$teta2sigmoid)
+    svm.predict <- predict(svm.model,testSplit)
+  }
+  
+  numsvm=as.numeric(svm.predict)
+  numsvm[numsvm == 1] <- 0
+  numsvm[numsvm == 2] <- 1
+  
+  costsvm=cost_model(numsvm,numtest, testSplit$Amount, fixedcost=10)
+  
+  svm.auc <- auc(roc(testSplit$Class,as.numeric(svm.predict)))
+  svm.gini <- 2*svm.auc-1
+  
+  cm1 <- confusionMatrix(svm.predict,testSplit$Class)
+  
+  par(mar=c(2,2,2,2))
+  draw_confusion_matrix <- function(cmtrx,auc,gini,loss) {
+    total <- sum(cmtrx$table)
+    
+    layout(matrix(c(1,1,2)))
+    par(mar=c(2,2,2,2))
+    plot(c(100, 345), c(300, 450), type = "n", xlab="", ylab="", xaxt='n', yaxt='n')
+    title('CONFUSION MATRIX')
+    
+    classes = colnames(cmtrx$table)
+    rect(150, 430, 240, 370, col="green")
+    text(195, 435, classes[1], cex=1.2)
+    rect(250, 430, 340, 370, col="red")
+    text(295, 435, classes[2], cex=1.2)
+    text(125, 370, 'Predicted', cex=1.3, srt=90, font=2)
+    text(245, 450, 'Actual', cex=1.3, font=2)
+    rect(150, 305, 240, 365, col="red")
+    rect(250, 305, 340, 365, col="green")
+    text(140, 400, classes[1], cex=1.2, srt=90)
+    text(140, 335, classes[2], cex=1.2, srt=90)
+    
+    text(195, 400,cm1$table[1,1], cex=1.6, font=2, col='white')
+    text(195, 335,cm1$table[1,2], cex=1.6, font=2, col='white')
+    text(295, 400,cm1$table[2,1], cex=1.6, font=2, col='white')
+    text(295, 335,cm1$table[2,2], cex=1.6, font=2, col='white')
+    
+    plot(c(100, 0), c(100, 0), type = "n", xlab="", ylab="", main = "DETAILS", xaxt='n', yaxt='n')
+    
+    text(10, 85, names(cmtrx$byClass[1]), cex=1.2, font=2)
+    text(10, 70, round(as.numeric(cmtrx$byClass[1]), 3), cex=1.2)
+    text(36, 85, names(cmtrx$byClass[2]), cex=1.2, font=2)
+    text(36, 70, round(as.numeric(cmtrx$byClass[2]), 3), cex=1.2)
+    text(62, 85, names(cmtrx$byClass[5]), cex=1.2, font=2)
+    text(62, 70, round(as.numeric(cmtrx$byClass[5]), 3), cex=1.2)
+    text(90, 85, names(cmtrx$byClass[7]), cex=1.2, font=2)
+    text(90, 70, round(as.numeric(cmtrx$byClass[7]), 3), cex=1.2)
+    
+    text(10, 35, names(cmtrx$overall[1]), cex=1.2, font=2)
+    text(10, 20, round(as.numeric(cmtrx$overall[1]), 3), cex=1.2)
+    text(36, 36, "AUC", cex=1.2, font=2)
+    text(36, 20, round(as.numeric(auc), 3), cex=1.2)
+    text(62, 36, "Gini", cex=1.2, font=2)
+    text(62, 20, round(as.numeric(gini), 3), cex=1.2)
+    text(90, 36, "Loss", cex=1.2, font=2)
+    text(90, 20, round(as.numeric(loss), 3), cex=1.2)
+  }
+  draw_confusion_matrix(cm1,svm.auc,svm.gini,costsvm)
+  
+})
 
 
 y <- reactive({input$compare})
@@ -315,31 +398,42 @@ output$mesure <- renderTable({
   cm2 <- confusionMatrix(as.factor(fitted.results),testSplit$Class)
   cm3 <- confusionMatrix(knn.model,testSplit$Class)
   cm4 <- confusionMatrix(rf.pred,testSplit$Class)
+  
+  svm.auc <- auc(roc(testSplit$Class,as.numeric(svm.predict)))
+  glm.auc <- auc(roc(testSplit$Class,as.numeric(fitted.results)))
+  knn.auc <- auc(roc(testSplit$Class,as.numeric(knn.model)))
+  rf.auc <- auc(roc(testSplit$Class,as.numeric(rf.pred)))
+  
+  svm.gini <- 2*svm.auc-1
+  glm.gini <- 2*glm.auc-1
+  knn.gini <- 2*knn.auc-1
+  rf.gini <- 2*rf.auc-1
 
 
   if (y()=="Logistic regression"){
-  q=cbind(Method="SVM",round(t(cm1$byClass[c(1,2,5,11)]),digits=4),Gini=round(ineq(svm.predict,type="Gini"),digits=4),Loss=costsvm)
-  s=cbind("Logistic regression",round(t(cm2$byClass[c(1,2,5,11)]),digits=4),round(ineq(fitted.results,type="Gini"),digits=4),costfit)
+  q=cbind(Method="SVM",round(t(cm1$byClass[c(1,2,5,11)]),digits=4),Gini=round(svm.gini,digits=4),Loss=costsvm)
+  s=cbind("Logistic regression",round(t(cm2$byClass[c(1,2,5,11)]),digits=4),round(glm.gini,digits=4),costfit)
   rbind(q,s)
   }
 
   else if (y()=="KNN"){
-  q=cbind(Method="SVM",round(t(cm1$byClass[c(1,2,5,11)]),digits=4),Gini=round(ineq(svm.predict,type="Gini"),digits=4),Loss=costsvm)
-  s=cbind("KNN",round(t(cm3$byClass[c(1,2,5,11)]),digits=4),round(ineq(knn.model,type="Gini"),digits=4),costknn)
+  q=cbind(Method="SVM",round(t(cm1$byClass[c(1,2,5,11)]),digits=4),Gini=round(svm.gini,digits=4),Loss=costsvm)
+  s=cbind("KNN",round(t(cm3$byClass[c(1,2,5,11)]),digits=4),round(knn.gini,digits=4),costknn)
   rbind(q,s)
   }
   
   else if (y()=="Random forest"){
-  q=cbind(Method="SVM",round(t(cm1$byClass[c(1,2,5,11)]),digits=4),Gini=round(ineq(svm.predict,type="Gini"),digits=4),Loss=costsvm)
-  s=cbind("Random forest",round(t(cm4$byClass[c(1,2,5,11)]),digits=4),round(ineq(rf.pred,type="Gini"),digits=4),costrf)
+  q=cbind(Method="SVM",round(t(cm1$byClass[c(1,2,5,11)]),digits=4),Gini=round(svm.gini,digits=4),Loss=costsvm)
+  s=cbind("Random forest",round(t(cm4$byClass[c(1,2,5,11)]),digits=4),round(rf.gini,digits=4),costrf)
   rbind(q,s)
   }
   
   else if (y()=="All"){
-    q=cbind(Method="SVM",round(t(cm1$byClass[c(1,2,5,11)]),digits=4),Gini=round(ineq(svm.predict,type="Gini"),digits=4),Loss=costsvm)
-    k=cbind("Logistic regression",round(t(cm2$byClass[c(1,2,5,11)]),digits=4),round(ineq(fitted.results,type="Gini"),digits=4),costfit)
-    l=cbind("KNN",round(t(cm3$byClass[c(1,2,5,11)]),digits=4),round(ineq(knn.model,type="Gini"),digits=4),costknn)
-    m=cbind("Random forest",round(t(cm4$byClass[c(1,2,5,11)]),digits=4),round(ineq(rf.pred,type="Gini"),digits=4),costrf)
+
+    q=cbind(Method="SVM",round(t(cm1$byClass[c(1,2,5,11)]),digits=4),Gini=round(svm.gini,digits=4),Loss=costsvm)
+    k=cbind("Logistic regression",round(t(cm2$byClass[c(1,2,5,11)]),digits=4),round(glm.gini,digits=4),costfit)
+    l=cbind("KNN",round(t(cm3$byClass[c(1,2,5,11)]),digits=4),round(knn.gini,digits=4),costknn)
+    m=cbind("Random forest",round(t(cm4$byClass[c(1,2,5,11)]),digits=4),round(rf.gini,digits=4),costrf)
     rbind(q,k,l,m)
   }
 })
